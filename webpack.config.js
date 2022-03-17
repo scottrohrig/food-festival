f7af921329fb43e651cd12fbcca17fa66bb546bd
Scott Rohrig <scott.rohrig@gmail.com>
2022-03-17 15:02:34 -0700

    modularaize entry points and add bundle analyzer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const webpack = require( 'webpack' );
 const path = require( 'path' );
+const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js',
+    },
     output: {
         path: path.resolve( __dirname, 'dist' ),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js'
     },
     plugins: [
         new webpack.ProvidePlugin( {
             $: 'jquery',
             jQuery: 'jquery'
         } ),
+        new BundleAnalyzerPlugin( {
+            // the report outputs to an HTML file in the dist folder
+            analyzerMode: 'static',
+        } )
     ],
     mode: 'development'
 };
