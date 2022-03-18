852462d1ffd5af680fb889d8faf4c46d3b8b0b0c
scottrohrig <scott.rohrig@gmail.com>
2022-03-17 19:06:58 -0700

    optimize images with img packages

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,49 @@ const path = require( 'path' );
 const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
 
 module.exports = {
-    entry: {
-        app: './assets/js/script.js',
-        events: './assets/js/events.js',
-        schedule: './assets/js/schedule.js',
-        tickets: './assets/js/tickets.js',
-    },
-    output: {
-        path: path.resolve( __dirname, 'dist' ),
-        filename: '[name].bundle.js'
-    },
-    plugins: [
-        new webpack.ProvidePlugin( {
-            $: 'jquery',
-            jQuery: 'jquery'
-        } ),
-        new BundleAnalyzerPlugin( {
-            // the report outputs to an HTML file in the dist folder
-            analyzerMode: 'static',
-        } )
-    ],
-    mode: 'development'
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js',
+  },
+  output: {
+    path: path.resolve( __dirname, 'dist' ),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name( file ) {
+                return "[path][name].[ext]";
+              },
+              publicPath: function ( url ) {
+                return url.replace( '../', '/assets/' );
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin( {
+      $: 'jquery',
+      jQuery: 'jquery'
+    } ),
+    new BundleAnalyzerPlugin( {
+      // the report outputs to an HTML file in the dist folder
+      analyzerMode: 'static',
+    } )
+  ],
+  mode: 'development'
 };
